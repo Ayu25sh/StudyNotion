7e6d46e83157866399abe07e96f7da280d7251f8
Your Name <khuranaayush08@gmail.com>
2024-09-20 22:29:37 +0530

    made some changes

--- src/services/operations/courseDetailsAPI.js
+++ src/services/operations/courseDetailsAPI.js
@@ -87,12 +87,12 @@ export const addCourseDetails = async (data, token) => {
   let result = null
   const toastId = toast.loading("Loading...")
   try {
-    console.log("before hit createCourse")
+    console.log("before hit createCourse",data);
     const response = await apiConnector("POST", CREATE_COURSE_API, data, {
       "Content-Type": "multipart/form-data",
-      Authorisation: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     })
-    console.log("after hit createCourse")
+    console.log("after hit createCourse",data)
     console.log("CREATE COURSE API RESPONSE............", response)
     if (!response?.data?.success) {
       throw new Error("Could Not Add Course Details")
