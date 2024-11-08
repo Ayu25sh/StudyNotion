90c64808e6560c5a0168fa502eef10f7f77a5ba9
Your Name <khuranaayush08@gmail.com>
2024-11-08 22:00:14 +0530

    fixed the styling

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div className='flex items-center justify-center text-white'>
+    <div className='flex items-center justify-center text-white text-3xl'>
         Error - 404 Not found
     </div>
   )
