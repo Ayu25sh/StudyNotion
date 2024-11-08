f1bbb84416d8eddcb3790e0408ad5725f2bd4f91
Your Name <khuranaayush08@gmail.com>
2024-11-08 21:53:08 +0530

    added styling

--- src/components/core/Dashboard/MyCourses.js
+++ src/components/core/Dashboard/MyCourses.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 import { fetchInstructorCourses } from '../../../services/operations/courseDetailsAPI';
 import { useNavigate } from 'react-router-dom';
 import IconBtn from '../../common/IconBtn';
+import { VscAdd } from "react-icons/vsc"
+
 
 import CoursesTable from './InstructorCourses/CoursesTable';
 
@@ -23,15 +25,16 @@ export default function MyCourses() {
     },[])
 
   return (
-    <div className='text-richblack-5'>
-        <div>
-            <h1>My Courses</h1>
-            <IconBtn 
+    <div>
+        <div className="mb-14 flex items-center justify-between">
+            <h1 className="text-3xl font-medium text-richblack-5">My Courses</h1>
+            <IconBtn
                 onClick={ () => navigate("/dashboard/add-course") }
                 text= "Add Course"
                 icon={true}
-
-            />
+            >
+                <VscAdd />
+            </IconBtn>
         </div>
 
         {courses && <CoursesTable courses= {courses} setCourses= {setCourses}/>}
