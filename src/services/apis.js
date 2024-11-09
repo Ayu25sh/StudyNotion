9ec00e028be845d627a512088159ef77e7832a67
Your Name <khuranaayush08@gmail.com>
2024-11-09 21:11:14 +0530

    added remaining apis

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,19 +1,4 @@
-
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000/api/v1"
-
-// export const categories = {
-//     CATEGORIES_API: BASE_URL + "/course/showAllCategories"
-// }
-
-//AUTH ENDPOINTS
-// export const endpoint = {
-//     SENDOTP_API: BASE_URL + "/auth/sendotp",
-//     SIGNUP_API: BASE_URL + "/auth/signup",
-//     LOGIN_API: BASE_URL + "/auth/login",
-//     RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
-//     RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token"
-// }
-
 
 
 // Auth Endpoints
@@ -23,13 +8,20 @@ export const authApi = {
     POST_LOGIN_USER_API: BASE_URL + '/auth/login',
     POST_RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
     POST_RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token"    
-  }
+}
 
 // PROFILE ENDPOINTS
 export const profileEndpoints = {
   GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
   GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
   GET_INSTRUCTOR_DATA_API: BASE_URL + "/profile/instructorDashboard",
+}
+
+// STUDENTS ENDPOINTS
+export const studentEndpoints = {
+  COURSE_PAYMENT_API: BASE_URL + "/payment/capturePayment",
+  COURSE_VERIFY_API: BASE_URL + "/payment/verifyPayment",
+  SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/payment/sendPaymentSuccessEmail",
 }
 
 // COURSE ENDPOINTS
@@ -54,13 +46,30 @@ export const courseEndpoints = {
 }
 
 
+// RATINGS AND REVIEWS
+export const ratingsEndpoints = {
+  REVIEWS_DETAILS_API: BASE_URL + "/course/getReviews",
+}
 
+// CATAGORIES API
+export const categories = {
+  CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+}
+
+// CATALOG PAGE DATA
+export const catalogData = {
+  CATALOGPAGEDATA_API: BASE_URL + "/course/getCategoryPageDetails",
+}
+// CONTACT-US API
+export const contactusEndpoint = {
+  CONTACT_US_API: BASE_URL + "/reach/contact",
+}
 
 // SETTINGS PAGE API
 export const settingsEndpoints = {
   UPDATE_DISPLAY_PICTURE_API: BASE_URL + "/profile/updateDisplayPicture",
   UPDATE_PROFILE_API: BASE_URL + "/profile/updateProfile",
-  CHANGE_PASSWORD_API: BASE_URL + "/auth/changePassword",
+  CHANGE_PASSWORD_API: BASE_URL + "/auth/changepassword",
   DELETE_PROFILE_API: BASE_URL + "/profile/deleteProfile",
 }
   
